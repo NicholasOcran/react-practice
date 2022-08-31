d4e660905a7a11c330e59e25ce181d238f19de75
nicocran1911 <93523661+nicocran1911@users.noreply.github.com>
2022-08-31 16:45:11 +0000

    Add files via upload

---
+++ react.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+
+const page = (
+  <div>
+    <h1>Am here</h1>
+    <h2>Yes we know</h2>
+  </div>
+);
+
+ReactDOM.render(page, document.getElementById)
